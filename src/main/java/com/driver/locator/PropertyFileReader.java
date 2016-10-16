1ab57bd184dd7f28254f56e8dafcf718de0002dd
Rahul Singh Rathore <Rahul Singh>
2016-10-16 22:21:40 +0800

    :+1: Added the projector structure

---
+++ src/main/java/com/driver/locator/PropertyFileReader.java
@@ -0,0 +1,42 @@
+package com.driver.locator;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import com.driver.locator.utility.ResourceHelper;
+
+public class PropertyFileReader {
+	
+	private Properties aProperty = null;
+	private String bFileName = "";
+	
+	private String getFilePath(){
+		return this.getClass().getResource("/").getPath() + "resources/configfile/" + this.bFileName;
+	}
+	
+	public PropertyFileReader() {
+		this.bFileName = "config.properties";
+		
+		try {
+				aProperty = new Properties();
+				aProperty.load(ResourceHelper.getResourcePathInputStream("resources/configfile/" + this.bFileName));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public PropertyFileReader(String cConfigPropFileName) {
+		this.bFileName = cConfigPropFileName;
+		
+		try {
+				aProperty = new Properties();
+				aProperty.load(new FileInputStream(getFilePath()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String[] getWebsiteNames() {
+		return aProperty.getProperty("Website").split(",");
+	}
+}
---
+++ src/main/java/com/driver/locator/utility/ElementLocator.java
@@ -0,0 +1,5 @@
+package com.driver.locator.utility;
+
+public class ElementLocator {
+
+}
