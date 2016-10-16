52c57e4988bd3e31c66e7e560453f610f6c9639c
Rahul Singh Rathore <Rahul Singh>
2016-10-16 18:36:43 +0800

    :+1: Added the maven project

---
+++ src/main/java/com/driver/locator/App.java
@@ -0,0 +1,13 @@
+package com.driver.locator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
